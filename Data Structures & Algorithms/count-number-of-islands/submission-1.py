b94f929ea376a9655eb0f473c52442ba9a4d92e0
Andrew Durnford <134669405+andrewhdurnford@users.noreply.github.com>
2026-05-08 15:03:56 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        res = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def bfs(r, c):
+            nonlocal res
+            if grid[r][c] == '0':
+                return
+
+            grid[r][c] = '0'
+
+            for dr, dc in dirs:
+                if (min(r + dr, c + dc) < 0
+                    or r + dr == ROWS
+                    or c + dc == COLS):
+                    continue
+                
+                bfs(r + dr, c + dc)
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1':
+                    res += 1
+                    bfs(r, c)
+        
+        return res
+            
