df6d020d6197efaefe7dcc6a2ba543561260c07b
Andrew Durnford <134669405+andrewhdurnford@users.noreply.github.com>
2026-05-22 12:22:32 -0400

    Add: non-overlapping-intervals - submission-11

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        res = 0
+        intervals.sort()
+        cur = intervals[0][1]
+        print(intervals)
+        for i in range(1, len(intervals)):
+            if cur <= intervals[i][0]:
+                cur = intervals[i][1]
+            else:
+                res += 1
+                cur = min(cur, intervals[i][1])
+        
+        return res
+        
