2ffbf4f5e0b1d804ee1a15a0d3a463ee8e34e871
Andrew Durnford <134669405+andrewhdurnford@users.noreply.github.com>
2026-05-27 19:38:20 -0400

    Add: network-delay-time - submission-6

---
+++ Data Structures & Algorithms/network-delay-time/submission-6.py
@@ -0,0 +1,33 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        neis = [[] for _ in range(n + 1)]
+
+        for source, target, t in times:
+            neis[source].append((t, target))
+        
+        # djikstra's algorithm!!!
+        # heap + visit set
+        # init heap with k
+        # at each node:
+        # mark as visited
+        # push all unvisited neighbors + time
+        # heap item sohuld be (cumulative cost, node)
+
+        heap = []
+        heapq.heappush(heap, (0, k))
+        visit = set()
+        res = 0
+
+        while heap:
+            t, cur = heapq.heappop(heap)
+            if cur in visit:
+                continue
+            visit.add(cur)
+            res = max(res, t)
+
+            for nei in neis[cur]:
+                if nei[1] not in visit:
+                    item = [nei[0] + t, nei[1]]
+                    heapq.heappush(heap, tuple(item))
+        
+        return res if len(visit) == n else -1
